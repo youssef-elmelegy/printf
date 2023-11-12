c26463e0d63221a9635d61b34cc7a9dc2ffa2b50
amr <amramr9016@gmail.com>
2023-11-12 12:36:28 -0800

    djskd

--- _printf.c
+++ _printf.c
@@ -13,9 +13,8 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (*format == NULL)
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-
 	while (*format)
 	{
 		if (*format == '%' && *(format + 1))
