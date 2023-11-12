73183c855fc9c41909b0821237a0240c5dfd6aff
amr <amramr9016@gmail.com>
2023-11-12 10:16:37 -0800

    3mr

--- _putchar.c
+++ _putchar.c
@@ -3,10 +3,10 @@
 /**
  * _putchar - writes a character
  * @c: The character to print
- * 
+ *
  * Return: On success 1. /On error -1 is returned.
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
