4a383c25c511bf42c3c1d197bde73e06b48f932c
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-11-12 13:42:44 -0800

    done

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 	while (*format)
 	{
