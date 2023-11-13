d40fa165f163fc2efbf7cac006251916c631fe72
youssef elmelegy <youssefelmelegy999@gmail.com>
2023-11-13 10:05:00 -0800

    update

--- _printf.c
+++ _printf.c
@@ -20,25 +20,7 @@ int _printf(const char *format, ...)
 		if (*format == '%' && *(format + 1))
 		{
 			format++;
-			switch (*format)
-			{
-				case 'c':
-					count = print_char(args, count);
-					break;
-				case 's':
-					count = print_string(args, count);
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				case 'd':
-				case 'i':
-					count = print_number(args, count);
-					break;
-				default:
-					count += _putchar('%') + _putchar(*format);
-					break;
-			}
+			count = all_spacifiers(format, args, count);
 		}
 		else
 			count += _putchar(*format);
---
+++ _spacifiers.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * all_spacifiers - print formats
+ * @format: a apacifier format
+ * @args: va_list
+ * @count: a returned count
+ * Return: count
+ */
+
+int all_spacifiers(char *format, va_list args, int count)
+{
+	switch (*format)
+	{
+		case 'c':
+			count = print_char(args, count);
+			break;
+		case 's':
+			count = print_string(args, count);
+			break;
+		case '%':
+			count += _putchar('%');
+			break;
+		case 'd':
+		case 'i':
+			count = print_number(args, count);
+			break;
+		case 'o':
+			count = print_octal(args, count);
+			break;
+		case 'x':
+			count = print_hex(args, count);
+			break;
+		case 'X':
+			count = print_HEX(args, count);
+			break;
+		default:
+			count += _putchar('%') + _putchar(*format);
+			break;
+	}
+	return (count);
+}
+
